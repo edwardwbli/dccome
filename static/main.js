d9085a5b343166f84623ee0c1a5e80431507d1e8
edwardwbli <edward.w.b.li@gmail.com>
2016-07-27 15:19:22 +0800

    comment

--- static/main.js
+++ static/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+    //function to support keyup event in #txt_name
     $("#txt_name").keyup(function(){
 	var encoded = encodeURIComponent($(this).val());
 	$.get( "/convert?s="+encoded, function( data ) {
 	    $( "#qrcode-container" ).html( data );
 	});
     });
+
+    //other function
 
 });
