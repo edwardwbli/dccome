18154edde2f67aac3ba103095f46114ecb1d9df9
edwardwbli <edward.w.b.li@gmail.com>
2016-07-27 11:48:15 +0800

    add templates

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/convert/<str>')
 def convert(str):
-
+    print str
     img = pyqrcode.create(str,encoding='utf8')
     buffer = StringIO()
     img.svg(buffer,scale=4)
