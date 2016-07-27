fd7422d1cb80109df8ed30cbd29b3d17eb0f713b
edwardwbli <edward.w.b.li@gmail.com>
2016-07-27 10:49:48 +0800

    qrcode initialize

--- hello.py
+++ hello.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
+
+import pyqrcode
+import re
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('webpage.html')
+
+@app.route('/convert/<str>')
+def convert(str):
+
+    img = pyqrcode.create(str,encoding='utf8')
+    buffer = StringIO()
+    img.svg(buffer,scale=4)
+    p = re.compile(r'<svg.*</svg>')
+    svgsegment = re.search(p, buffer.getvalue())
+    testsvg = svgsegment.group(0)
+    return testsvg
+
 
 
 if __name__ == '__main__':
