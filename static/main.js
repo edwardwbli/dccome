1555bfe0a3b0f045d932e2caef1a9e33a7cf3f4f
edwardwbli <edward.w.b.li@gmail.com>
2016-09-04 13:35:13 +0800

    Change event

--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //function to support keyup event in #txt_name
-    $("#txt_name").keyup(function(){
+    $("#txt_name").change(function(){
 	var encoded = encodeURIComponent($(this).val());
 	$.get( "/convert?s="+encoded, function( data ) {
 	    $( "#qrcode-container" ).html( data );
